5bfd3588b3aa0ad36cfe95d0dc5fdd9daa3150ca
Sergey-Navitskiy <126409337+Sergey-Navitskiy@users.noreply.github.com>
2023-09-10 21:29:24 +0300

    begining

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
+
+
+
+const requst = new XMLHttpRequest()
+requst.open('GET', "https://meowfacts.herokuapp.com/?count=3")
+requst.send()
+
+requst.addEventListener('load', function(){
+    const data = JSON.parse(requst.responseText)
+    console.log(data)
+    const [text] = data.data
+    console.log(text)
+})
+
+console.log('hello world')
