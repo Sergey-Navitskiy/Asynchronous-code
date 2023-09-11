ded43ff9ce9221ffade54c890d4055024c4f5582
Sergey-Navitskiy <126409337+Sergey-Navitskiy@users.noreply.github.com>
2023-09-11 19:52:36 +0300

    countries XML

--- script.js
+++ script.js
@@ -4,18 +4,28 @@ const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
+function getCountryDAta(country) {
+  const requst = new XMLHttpRequest();
+  requst.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  requst.send();
 
+  requst.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    const html = `<article class="country">
+        <img class="country__img" src="${data.flags.svg}" />
+        <div class="country__data">
+          <h3 class="country__name">${data.name.common}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${data.population}</p>
+          <p class="country__row"><span>🗣️</span>${data.languages.rus}</p>
+          <p class="country__row"><span>💰</span>${data.currencies.RUB.name}</p>
+        </div>
+      </article>`;
 
-
-const requst = new XMLHttpRequest()
-requst.open('GET', "https://meowfacts.herokuapp.com/?count=3")
-requst.send()
-
-requst.addEventListener('load', function(){
-    const data = JSON.parse(requst.responseText)
-    console.log(data)
-    const [text] = data.data
-    console.log(text)
-})
-
-console.log('hello world')
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+    countriesContainer.style.opacity = 1;
+  });
+}
+getCountryDAta('russia  ')
+console.log("hello world");
