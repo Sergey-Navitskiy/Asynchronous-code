7850d1581f6ffbcbf0ba9431162da375c1a52f6a
Sergey-Navitskiy <126409337+Sergey-Navitskiy@users.noreply.github.com>
2023-09-14 22:05:42 +0300

    fetch

--- script.js
+++ script.js
@@ -4,47 +4,72 @@ const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
-function getCountryDAta(country) {
-  const requst = new XMLHttpRequest();
-  requst.open("GET", `https://restcountries.com/v3.1/name/${country}`);
-  requst.send();
+// function getCountryDAta(country) {
+//   const requst = new XMLHttpRequest();
+//   requst.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+//   requst.send();
 
+//   function renderCards(data, className = ''){
+//     const html = `<article class="country ${className}">
+//         <img class="country__img" src="${data.flags.svg}" />
+//         <div class="country__data">
+//           <h3 class="country__name">${data.name.common}</h3>
+//           <h4 class="country__region">${data.region}</h4>
+//           <p class="country__row"><span>👫</span>${data.population}</p>
+//           <p class="country__row"><span>🗣️</span>${
+//             Object.entries(data.languages)[0][1]
+//           }</p>
+//           <p class="country__row"><span>💰</span>${
+//             Object.entries(Object.entries(data.currencies)[0][1])[0][1]
+//           } </p>
+//         </div>
+//       </article>`;
 
-  function renderCards(data, className = ''){
-    const html = `<article class="country ${className}">
-        <img class="country__img" src="${data.flags.svg}" />
-        <div class="country__data">
-          <h3 class="country__name">${data.name.common}</h3>
-          <h4 class="country__region">${data.region}</h4>
-          <p class="country__row"><span>👫</span>${data.population}</p>
-          <p class="country__row"><span>🗣️</span>${
-            Object.entries(data.languages)[0][1]
-          }</p>
-          <p class="country__row"><span>💰</span>${
-            Object.entries(Object.entries(data.currencies)[0][1])[0][1]
-          } </p>
-        </div>
-      </article>`;
+//     countriesContainer.insertAdjacentHTML("beforeend", html);
+//     countriesContainer.style.opacity = 1;
+//   }
 
-    countriesContainer.insertAdjacentHTML("beforeend", html);
-    countriesContainer.style.opacity = 1;
-  }
+//   requst.addEventListener("load", function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+//     const neibhour = data.borders[0]
+//     renderCards(data)
 
+//     const request2 = new XMLHttpRequest()
+//     request2.open('GET', `https://restcountries.com/v3.1/alpha/${neibhour}`)
+//     request2.send()
+//     request2.addEventListener('load', function(){
+//       const [data2] = JSON.parse(this.responseText);
+//       renderCards(data2,'neighbour')
+//     })
+//   });
+// }
+// getCountryDAta("usa");
+// console.log("hello world");
 
-  requst.addEventListener("load", function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-    const neibhour = data.borders[0]
-    renderCards(data)
+// fetch
 
-    const request2 = new XMLHttpRequest()
-    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neibhour}`)
-    request2.send()
-    request2.addEventListener('load', function(){
-      const [data2] = JSON.parse(this.responseText);
-      renderCards(data2,'neighbour')
-    })
-  });
+function renderCards(data, className = "") {
+  const html = `<article class="country ${className}">
+          <img class="country__img" src="${data.flags.svg}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${data.population}</p>
+            <p class="country__row"><span>🗣️</span>${
+              Object.entries(data.languages)[0][1]
+            }</p>
+            <p class="country__row"><span>💰</span>${
+              Object.entries(Object.entries(data.currencies)[0][1])[0][1]
+            } </p>
+          </div>
+        </article>`;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
 }
-getCountryDAta("usa");
-console.log("hello world");
+
+function getCountryDAta(country) {
+  const req = fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then((response) => response.json())
+    .then((data) => renderCards(data[0]));
+}
